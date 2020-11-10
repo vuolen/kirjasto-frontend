74a0e2196e547122d5df82a71ffcd1ddf5f114a9
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-11-10 23:04:45 +0200

    added fetching of /books endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { from } from "rxjs";
+import { map } from "rxjs/operators";
 
-const App = () => (
-    <div>
-        Hello world!
-    </div>
+const BACKEND_HOST = "http://localhost"
+const BACKEND_PORT = "8000"
+const BACKEND_BASE_URL = BACKEND_HOST + ":" + BACKEND_PORT
+
+const book$ = from(axios.get(BACKEND_BASE_URL + "/books")).pipe(
+    map(res => res.data)
 )
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const App = () => {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        book$.subscribe(
+            setBooks,
+            err => console.log("Couldnt get /books")
+        )
+    }, [])
+    console.log(books)
+    return <div>
+        {JSON.stringify(books)}
+    </div>
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
